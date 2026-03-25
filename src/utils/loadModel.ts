951d4d26446d36e6792b62d979d7b4a30059c48a
suraj4507-svg <suraj4507svg@example.com>
2026-03-25 12:27:43 +0530

    Fix: Correct syntax and model type in loadModel utility

--- src/utils/loadModel.ts
+++ src/utils/loadModel.ts
@@ -2,7 +2,7 @@ import * as tf from "@tensorflow/tfjs"
 
 export async function loadModel(){
 
-const model = await tf.loadLayersModel(/model/model.json)
+const model = await tf.loadGraphModel("/model/model.json")
 
 return model
 
